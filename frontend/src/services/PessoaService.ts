709e10c370f965ca615cb6402de83ee5321e2cdb
samcamargoo <sam6camargo@gmail.com>
2022-10-15 10:31:54 -0300

    mudança no endpoint para utilizar o heroku

--- frontend/src/services/PessoaService.ts
+++ frontend/src/services/PessoaService.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
-import { AiOutlineException } from "react-icons/ai";
-import { Pessoa } from "../models/Pessoa";
+import axios from 'axios';
 
-const API_URL = "http://localhost:8080/api/v1/"
-const VERIFICAR_EMAIL_URL = "http://localhost:8080/api/v1/verificar-email"
+import { Pessoa } from '../models/Pessoa';
+
+const API_URL = "https://rocky-fortress-14961.herokuapp.com/api/v1/"
+const VERIFICAR_EMAIL_URL = "https://rocky-fortress-14961.herokuapp.com/api/v1/verificar-email"
 
 export function cadastrarPessoa (pessoa: Pessoa) {
     return axios.post(API_URL + "pessoa", pessoa)
